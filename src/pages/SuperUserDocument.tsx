b8288e0d29b3b9b29c727a0c8e95bc8b8432a22d
Dieumerci Muhindo <dieumerci.md09@gmail.com>
2023-08-19 22:58:04 +0200

    feat : add pages configurations

---
+++ src/pages/SuperUserDocument.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+const SuperUserDocument = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default SuperUserDocument
---
+++ src/pages/UserDocument.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const UserDocument = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default UserDocument
