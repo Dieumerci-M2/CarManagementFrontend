f9c9221f2a5504e5e29dab5d603da8ced9d00635
Dieumerci Muhindo <dieumerci.md09@gmail.com>
2023-08-21 22:40:19 +0200

    feat : add routes to the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,29 @@
 
 import './App.css'
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Presentation from './pages/Presentation';
+import SuperUserDocument from './pages/SuperUserDocument';
+import SuperUserLogin from './pages/SuperUserLogin';
+import SuperUserSingup from './pages/SuperUserSingup';
+import UserDocument from './pages/UserDocument';
+import UserSingUp from './pages/UserSingUp';
+import UserLogin from './pages/UserLogin';
 
 function App() {
   
 
   return (
-    <>
-      <div>
-       <h1 className="text-3xl font-bold underline">Hello md</h1>
-      </div>
-    </>
+  
+    <Router>
+        <Route path='/' element={ <Presentation />} />
+      <Route path='/user-login' element={<UserLogin />} />
+      <Route path='/super-user-login' element={<SuperUserLogin />} />
+      <Route path='/user-signUp' element={<UserSingUp />} />
+      <Route path='/super-user-signUp' element={<SuperUserSingup />} />
+      <Route path='/user-document' element={<UserDocument />} />
+      <Route path='/super-user-document' element={<SuperUserDocument />} />
+    </Router>
+    
   )
 }
 
