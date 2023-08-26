38e4dfacc6d8f048958c84fe444b7e5184ae5161
Dieumerci Muhindo <dieumerci.md09@gmail.com>
2023-08-26 22:49:18 +0200

    feat add super user Document

--- src/pages/SuperUserDocument.tsx
+++ src/pages/SuperUserDocument.tsx
@@ -1,9 +1,30 @@
+import Document from "../components/Document"
+import { Input, Space } from 'antd';
+import '../App.css'
 
 const SuperUserDocument = () => {
+  const { Search } = Input;
+  const onSearch = (value: string) => console.log(value);
   return (
-    <div>
-      
-    </div>
+   <article>
+      <section className="flex justify-center items-center gap-2 my-14">
+        <div>
+          <Space direction="vertical">
+           <Search placeholder="input search text" onSearch={onSearch} className="w-96" />
+          </Space>
+        </div>
+        <div>
+          <select name="cars" id="car">
+            <option value="plaque">Plaque</option>
+            <option value="dog">Nom Prop</option>
+            <option value="cat">Marque</option>
+          </select>
+        </div>
+      </section>
+      <section>
+          <Document />
+      </section>
+    </article>
   )
 }
 
