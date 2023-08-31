6bef0a90f9ca2c2690160039653775646889dad3
Dieumerci Muhindo <dieumerci.md09@gmail.com>
2023-08-31 10:52:35 +0200

    feat : add InfoDoc component to show only one document informations

--- src/pages/UserOneDocument.tsx
+++ src/pages/UserOneDocument.tsx
@@ -1,20 +1,22 @@
+import InfoDoc from "../components/InfoDoc"
+import car from "../assets/voiture.jpg"
+import man from "../assets/c.jpg"
 
 const UserOneDocument = () => {
   
   return (
-    <article className="flex justify-between p-10">
-      <section>
-        <div>
-          <img />
-          <span></span>
+    <article className="flex justify-between py-8 px-24">
+      <section className="flex flex-col justify-between">
+        <div className="flex flex-col gap-2">
+          <img src={ car} alt="Car" className="w-80 h-48"/>
+          <span>Marque : BMW</span>
         </div>
-        <div>
-          <img />
-          <span></span>
+        <div className="flex flex-col gap-2">
+          <img src={ man } alt="Owner" className="w-80 h-48"/>
+          <span>Md</span>
         </div>
       </section> 
       <section>
-        <span></span>
         <InfoDoc />
       </section>
     </article>
