fe1488c21ae3e4b7e16fc530d3b9a5d5ca2e5354
Dieumerci Muhindo <dieumerci.md09@gmail.com>
2023-09-03 20:58:57 +0200

    feat : add superuserDoc page structure

--- src/pages/SuperUserDocument.tsx
+++ src/pages/SuperUserDocument.tsx
@@ -1,17 +1,59 @@
-import Document from "../components/Document"
-import { Input, Space } from 'antd';
-import '../App.css'
+import { Input } from 'antd';
+import React, { useContext, useEffect, useState } from "react";
+import '../App.css';
+import Document from "../components/Document";
+import { UserDocumentInfo } from "../@types/global"
+import { DocContext } from '../Context/Context';
+
 
 const SuperUserDocument = () => {
+   // Search data
+  const [searchData, setSearchData] = useState<UserDocumentInfo[]>([])
+  const { user } = useContext(DocContext)
+    console.log(user.token)
   const { Search } = Input;
-  const onSearch = (value: string) => console.log(value);
+  const onSearch: React.ChangeEventHandler<HTMLInputElement> = (e) => {
+    fetch(`https://carmanagementbackend-production.up.railway.app/document/findAll?plaque=${e.target.value}`, {
+      headers: {
+        Authorization: `Bearer ${user.token}` ,
+        mode: "cors"
+      }
+    }).then(res => res.json())
+      .then(({ data }:{data:UserDocumentInfo[]}) => {
+        setSearchData(data ?? [])
+        console.log(searchData)
+      })
+      .catch(console.error)
+  };
+
+  // Preview Data
+  const [previewData, setPreviewData] = useState<UserDocumentInfo[]>([]);
+  useEffect(() => {
+    fetch("https://carmanagementbackend-production.up.railway.app/document/findAll", {
+      headers: {
+        Authorization: `Bearer ${user.token}`,
+        mode: "cors"
+      }
+
+    }).then(res => res.json())
+      .then(({ data }:{data:UserDocumentInfo[]}) => {
+        setPreviewData(data.slice(0,7))
+      })
+      .catch(console.error)
+  }, [])
+  
   return (
-   <article>
-      <section className="flex justify-center items-center gap-2 my-14">
-        <div>
-          <Space direction="vertical">
-           <Search placeholder="input search text" onSearch={onSearch} className="w-96" />
-          </Space>
+    <article>
+      <section className="flex justify-center items-center gap-2 my-10">
+        <div className="relative">
+          <Search placeholder="input search text" onChange={onSearch} className="w-96" />
+          {
+            searchData.length === 0
+              ? <></>
+            : <ul className="p-4 absolute top-8 left-0 right-0 border-2 border-t-0 flex flex-col gap-2">
+                {searchData.map(item => <li key={item.id} className=''>{item.plaque}</li>)}
+              </ul>
+          }
         </div>
         <div>
           <select name="cars" id="car">
@@ -21,8 +63,10 @@ const SuperUserDocument = () => {
           </select>
         </div>
       </section>
-      <section>
-          
+      <section className='flex flex-wrap mx-20 gap-16 justify-between'>
+        {
+          previewData.map(data => <Document key={data.id} data={data} />)
+        }
       </section>
     </article>
   )
